09140184691221aa60475bbbbc243f8deece6b55
Naoki Okada <46291090+okadan@users.noreply.github.com>
2021-07-24 23:13:02 +0900

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(home: Scaffold(body: Center(child: Text('Hello World')))));
+}
